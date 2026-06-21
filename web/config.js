2aa6377b8a09a474a6931ff4bbd22615b52fe5c3
Scott Campbell <scott@scottcampbellconsulting.com>
2026-06-21 06:21:53 +0000

    chore: point frontend at production API

--- web/config.js
+++ web/config.js
@@ -8,5 +8,5 @@
 //   window.APP_CONFIG = { apiBase: "https://water-api.scottcampbell.io" };
 //
 window.APP_CONFIG = {
-  apiBase: "http://localhost:8000"
+  apiBase: "https://water-api.scottcampbell.io"
 };
